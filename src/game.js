597bdec3167ccfd63c2cfd376aa9cf04ca2669e9
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-10-05 23:59:11 -0400

    segunda prueba pasada

--- src/game.js
+++ src/game.js
@@ -1,17 +1,19 @@
 class VacasToros{
 
-    constructor(){
+    // constructor(){
 
 
-    }
+    // }
     play(player1Code, player2Guess)
     {
-        if (player2Guess === player1Code) {
-            return "*";
+        let resultado =""
+        for(let i =0; i<player1Code.length; i++){
+
+            if (player2Guess[i] == player1Code[i]) resultado += "!";
         }
-        return "0";
+        return resultado;
     }
 }
-  export default VacasToros;
+export default VacasToros;
 
 
--- src/game.spec.js
+++ src/game.spec.js
@@ -1,10 +1,14 @@
 import VacasToros from "./game"
+
 describe("Vacas y toros", () => {
-
-
-  it("devuelve * su el usuario acierta", () => {
-    let game = new VacasToros;
-    expect(game.play("2", "2")).toEqual("*");
+  let game
+  beforeEach( () => {
+     game = new VacasToros()
   });
-
+  it("devuelve ! su el usuario acierta", () => {
+    expect(game.play("2", "2")).toEqual("!");
+  });
+  it("devolvera la misma cantidad de ! que tenga de aciertos",() => {
+    expect(game.play("21341", "22314")).toEqual("!!");
+  });
 });
